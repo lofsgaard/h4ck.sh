dca536f7026825947459f75d7d9f74a12f2d8e50
Andreas Løfsgaard <andreas@lofsgaard.com>
2020-11-16 20:09:02 +0100

    Debug env

--- core/settings.py
+++ core/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = False
+DEBUG = os.getenv("DEBUG")
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
 
 #DB
